c98ecb7fc0a6b4d6564b6072c66bb3bbe9bb790a
Hendeerson <hendeeerson@gmail.com>
2022-02-28 17:09:08 -0600

    follow UP

--- Node.py
+++ Node.py
@@ -1,18 +1,4 @@
 class Node:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.nextt= None
-
-    def getId(self):
-        return self.id
-
-    def getName(self):
-        return self.name
-
-    def setId(self, id):
-        self.id = id
-
-    def setName(self, name):
-        self.name = name                   
+    def __init__(self):
+        self.start                 
     
