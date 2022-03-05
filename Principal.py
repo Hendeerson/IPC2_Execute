2efb7f27a17b2a9e334061e116bad46218307550
Hendeerson <hendeeerson@gmail.com>
2022-03-05 09:53:56 -0600

    Follow up 6

--- Principal.py
+++ Principal.py
@@ -23,62 +23,6 @@ def xPath(ruta):
     #print(root)
     print('\n')
 
-    '''
-    #print('Etiquetas colores')
-    for e in root:
-        print('etiquetas', e.tag)
-        print('valor', e.text)
-        print(e.attrib, '\n')
-        print('Atributes')
-    for elements in root.findall('.//'):
-        print(elements.attrib)
-
-        print(elements.tag)
-        print(elements.text)'''
-
-# print('\n')
-# print('Values')
-    '''
-    #verdadero
-    for elements in root:
-        print(elements.tag, end =": ")
-        print(elements.attrib['nombre'])
-        print(elements.text)
-        
-        
-        #patron = elements[0]
-        for subelements in elements:
-            print('*', subelements.tag, end= ":")
-            print(subelements.text)
-            #patrones = subelements[0]
-            for p in subelements:
-                print('-', p.tag, end = ': ')
-                print(p.text)
-        print('\n')
-        '''
-
-    '''
-    for x in root.findall('.//'):
-        
-        print(x.tag, end=":")
-        
-        print(x.text, end="")
-        print(x.attrib)'''
-
-
-
-
-    '''
-    for reviewR in root:
-        nombre = reviewR.text.replace('\n', '')
-        floorListt.lastInserted(reviewR.attrib['nombre'])
-        R= reviewR[0]
-        print(nombre)
-        for rr in R:
-            #print('_',  rr.tag)
-            #floorListt.lastInsertedV(rr .attrib['R'])
-           print(rr.text)'''
-        
     content = open(ruta).read()
     floorArt = ET.fromstring(content)
     for floorArte in floorArt.iter("pisosArtesanales"):
